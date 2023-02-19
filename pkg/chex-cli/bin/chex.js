675f22ffe310b3baf03ef653a0a94f40f912531e
Joe Hildebrand <joe-github@cursive.net>
2023-02-19 14:55:53 -0700

    Switch to monorepo

--- pkg/chex-cli/bin/chex.js
+++ pkg/chex-cli/bin/chex.js
@@ -2,10 +2,19 @@
 /* eslint-disable no-console */
 
 import {Buffer} from 'buffer'
-import fs from 'fs'
-import {hexDump} from '../lib/index.js'
+import {Command} from 'commander'
+import fs from 'fs/promises'
+import {hexDump} from '@cto.af/chex'
 
-const args = process.argv.slice(2)
+const program = new Command()
+program
+  .argument('[file...]', 'File names to read or "-" for stdin')
+  .option('-c,--colors', 'Force colors on even if stdout is not a tty')
+  .option('-d,--dots', 'Use dots for more control characters, instead of Unicode representations')
+  .parse()
+
+const opts = program.opts()
+const {args} = program
 if (args.length === 0) {
   args.push('-')
 }
@@ -26,9 +35,10 @@ async function main(argv) {
     }
     const buf = fn === '-' ?
       await readStdin() :
-      new Uint8Array(await fs.promises.readFile(fn))
+      new Uint8Array(await fs.readFile(fn))
     console.log(hexDump(buf, {
-      colors: process.stdout.isTTY,
+      colors: opts.colors || process.stdout.isTTY,
+      dots: opts.dots,
     }))
   }
 }
